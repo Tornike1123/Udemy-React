86c9e32e50795030de7413f1aeaf2f2055112eae
Tornike1123 <toko36106@gmail.com>
2023-02-14 22:22:39 +0400

    253.Refresher/Practie: Part 2/2

--- 13.Redux-Async-App/src/store/cart-slice.js
+++ 13.Redux-Async-App/src/store/cart-slice.js
@@ -6,15 +6,16 @@ const cartSlice = createSlice({
 		items: [],
 		totalQuantity: 0,
 	},
-	reducer: {
+	reducers: {
 		addItemToCart(state, action) {
 			const newItem = action.payload;
 			const existingItem = state.items.find(
 				(item) => item.id === newItem.id
 			);
+			state.totalQuantity++;
 			if (!existingItem) {
 				state.items.push({
-					itemId: newItem,
+					id: newItem.id,
 					price: newItem.price,
 					quantity: 1,
 					totalPrice: newItem.price,
@@ -26,10 +27,19 @@ const cartSlice = createSlice({
 					existingItem.totalPrice + newItem.price;
 			}
 		},
-		removeItemFromCart() {},
+		removeItemFromCart(state, action) {
+			const id = action.payload;
+			const existingItem = state.items.find((item) => item.id === id);
+			state.totalQuantity--;
+			if (existingItem.quantity === 1) {
+				state.items = state.items.filter((item) => item.id !== id);
+			} else {
+				existingItem.quantity--;
+			}
+		},
 	},
 });
 
-export const uiActions = cartSlice.actions;
+export const cartActions = cartSlice.actions;
 
 export default cartSlice;
