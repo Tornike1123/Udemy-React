39d8ef213373c4e8b73a14411fbfaa210db0a34d
Tornike1123 <toko36106@gmail.com>
2023-02-21 21:36:53 +0400

    276.Navigating Programmatically

--- 14.SPA/src/pages/Home.jsx
+++ 14.SPA/src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+	const navigate = useNavigate();
+
+	const navigateHandler = () => {
+		navigate("./Products");
+	};
+
 	return (
 		<div>
 			<h1>Home Page </h1>
+
+			<button onClick={navigateHandler}>Navigate</button>
 		</div>
 	);
 };
