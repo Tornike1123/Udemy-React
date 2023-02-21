9c796fbab7c1319353e3fa8cf7a3513663bae033
Tornike1123 <toko36106@gmail.com>
2023-02-21 21:06:57 +0400

    274.Showing Error Pages with errorElement

--- 14.SPA/src/App.js
+++ 14.SPA/src/App.js
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products.jsx";
 import RootLayout from "./pages/Root";
+import Error from "./pages/Error.jsx";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <RootLayout />,
+		errorElement: <Error />,
 		children: [
 			{ path: "/", element: <Home /> },
 			{ path: "/products", element: <Products /> },
