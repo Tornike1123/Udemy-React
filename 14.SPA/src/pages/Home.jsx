bb40df712c60ef10b004d953d70663c9f059996d
Tornike1123 <toko36106@gmail.com>
2023-02-21 20:54:36 +0400

    273. Layouts & Nested Routes (Children Outlet)

--- 14.SPA/src/pages/Home.jsx
+++ 14.SPA/src/pages/Home.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Home = () => {
 	return (
 		<div>
 			<h1>Home Page </h1>
-			<p>
-				Go to <Link to="/products"> the list of home</Link>
-			</p>
 		</div>
 	);
 };
